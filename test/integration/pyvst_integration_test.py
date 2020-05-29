829056ff05071462f2c1ef1a2578f70b561363b0
Sergey <grechin.sergey@gmail.com>
2020-05-29 10:51:40 +0300

    removed obsolete test

--- test/integration/pyvst_integration_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-class PyvstIntegrationTest(unittest.TestCase):
-    """
-    Tests a simple setup involving running 1 synth vst through pyvst wrapper
-    """
-
-    def testWithAmsynth(self):
