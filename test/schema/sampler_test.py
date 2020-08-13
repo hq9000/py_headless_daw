32ec4e1a91097b738230c679dbb7dbd12dce79b3
Sergey Grechin <grechin.sergey@gmail.com>
2020-08-13 06:26:37 +0300

    [py_headless_daw-4-implement-sampler-track #4] finished writing mocked waveform provider

--- test/schema/sampler_test.py
+++ test/schema/sampler_test.py
@@ -1,5 +1,7 @@
 import unittest
-from typing import List
+from typing import List, Tuple
+
+import numpy as np
 
 from py_headless_daw.dto.waveform import Waveform
 from py_headless_daw.processing.stream.sampler import Sampler
@@ -9,13 +11,37 @@ from py_headless_daw.schema.host import Host
 from py_headless_daw.schema.unit import Unit
 from py_headless_daw.services.wave_data_provider import WaveformProviderInterface
 
+
 class MockedWaveformProvider(WaveformProviderInterface):
 
     def get_wave_data_by_file_path(self, file_path: str) -> Waveform:
-        file_path.split('_')
+        """
+        interprets the file path as:
 
-        # continue on this mock
+        numchannels_volume_lengthinsamples_samplerate, e.g. 2_0.123_100_44100 and generates a flat "wave"
+        of the given length (in samples) on a given sample rate
 
+        """
+
+        (num_channels, volume, length_in_samples, sample_rate) = self._parse_file_path(file_path)
+        data = np.zeros((num_channels, length_in_samples)) + volume
+        return Waveform(sample_rate, data)
+
+    def _parse_file_path(self, file_path: str) -> Tuple[int, float, int, int]:
+        elements = file_path.split('_')
+        if elements.count != 4:
+            raise Exception('the file path should have had exactly 3 underscore-delimited parts')
+
+        (num_channels, volume, length_in_samples, sample_rate) = (
+            int(elements[0]),
+            float(elements[1]),
+            int(elements[2]),
+            int(elements[3])
+        )
+
+        return num_channels, volume, length_in_samples, sample_rate
+
+        pass
 
 
 class SamplerTest(unittest.TestCase):
@@ -26,7 +52,7 @@ class SamplerTest(unittest.TestCase):
         unit = Unit(2, 0, 2, 0, host)
 
         clips: List[AudioClip] = self.generate_test_clips()
-        mocked_waveform_provider = self.create_mocked_waveform_provider()
+        mocked_waveform_provider = MockedWaveformProvider()
         strategy = Sampler(clips, mocked_waveform_provider)
         strategy.unit = unit
 
@@ -34,14 +60,5 @@ class SamplerTest(unittest.TestCase):
         interval.start_in_seconds = 0
         interval.end_in_seconds = 1
 
-
-
     def generate_test_clips(self) -> List[AudioClip]:
-        pass
-
-    def create_mocked_waveform_provider(self) -> WaveformProviderInterface:
-
-
-
-
         pass
