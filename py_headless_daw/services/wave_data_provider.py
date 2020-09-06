82c027035d5864c199a91b668313ed07eafb1c30
Sergey Grechin <grechin.sergey@gmail.com>
2020-09-06 08:38:56 +0300

    [py_headless_daw-4-implement-sampler-track #4] properly handling matrix shape and type when loading wave failes

--- py_headless_daw/services/wave_data_provider.py
+++ py_headless_daw/services/wave_data_provider.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 import scipy.io.wavfile
+import numpy as np
 
 from py_headless_daw.dto.waveform import Waveform
 
@@ -20,4 +21,16 @@ class WaveDataProvider(WaveformProviderInterface):
 
     def get_wave_data_by_file_path(self, file_path: str) -> Waveform:
         (sample_rate, data) = scipy.io.wavfile.read(file_path)
+        data = np.rot90(data)
+
+        divider = None
+        if data.dtype is np.dtype(np.int16):
+            divider = 32768
+        elif data.dtype is np.dtype(np.int32):
+            divider = 2147483648
+
+        if divider is not None:
+            float_data = data / divider
+            data = float_data.astype(np.float32)
+
         return Waveform(sample_rate, data)
