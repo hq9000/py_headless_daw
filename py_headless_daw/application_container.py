31003f0dec33bed6f6ff2a781aa683e9a65066c6
Sergey Grechin <grechin.sergey@gmail.com>
2020-09-08 10:46:00 +0300

    [py_headless_daw-7-example-project #7] untested logic in project renderer

--- py_headless_daw/application_container.py
+++ py_headless_daw/application_container.py
@@ -2,11 +2,11 @@ from dependency_injector import providers
 from dependency_injector.containers import DeclarativeContainer
 
 from py_headless_daw.compiler.project_compiler import InternalPluginProcessingStrategyFactory, ProjectCompiler
+from py_headless_daw.project.project_renderer import ProjectRenderer
 from py_headless_daw.services.wave_data_provider import WaveDataProvider
 
 
 class ApplicationContainer(DeclarativeContainer):
-
     waveform_provider = providers.Singleton(
         WaveDataProvider
     )
@@ -19,4 +19,9 @@ class ApplicationContainer(DeclarativeContainer):
     project_compiler = providers.Singleton(
         ProjectCompiler,
         internal_plugin_processing_strategy_factory=internal_plugin_processing_strategy_factory
+    )
+
+    project_renderer = providers.Singleton(
+        ProjectRenderer,
+        project_compiler=project_compiler
     )
--- py_headless_daw/demo/riding_on_bugs/riding_on_bugs.py
+++ py_headless_daw/demo/riding_on_bugs/riding_on_bugs.py
@@ -1,5 +1,8 @@
 import logging
 
+from py_headless_daw.project.project import Project
+from py_headless_daw.project.project_renderer import ProjectRenderer
+
 logger = logging.getLogger(__name__)
 
 
@@ -7,6 +10,15 @@ class RidingOnBugs:
 
     logger = logging.getLogger(__name__)
 
+    def __init__(self, renderer: ProjectRenderer):
+        self._renderer: ProjectRenderer = renderer
+
     def render(self, output_file: str):
         logger.info('started rendering RidingOnBugs demo to ' + output_file)
+
+        project = self.create_project()
+        self._renderer.render_to_file(project, output_file)
+
+
+    def create_project(self) -> Project:
         pass
