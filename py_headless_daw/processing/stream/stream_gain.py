f8bc380080a66f921df2560fd4b47040e5b0dd71
Sergey Grechin <grechin.sergey@gmail.com>
2020-09-22 05:56:29 +0300

    [py_headless_daw-5-envelopes #5] smoothening out gain changes

--- py_headless_daw/processing/stream/stream_gain.py
+++ py_headless_daw/processing/stream/stream_gain.py
@@ -22,16 +22,34 @@ class StreamGain(ProcessingStrategy):
         all_events = self._flatten_event_inputs(event_inputs)
         all_events = sorted(all_events, key=lambda event: event.sample_position)
 
+
+        new_desired_gain = self.gain
+
         for event in all_events:
             if isinstance(event, ParameterValueEvent):
                 event = cast(ParameterValueEvent, event)
                 if event.parameter_id == self.PARAMETER_GAIN:
-                    self.gain = event.value
+                    new_desired_gain = event.value
+
+        if new_desired_gain != self.gain:
+            # even in this simplified approach,
+            # we still want to avoid abrupt volume changes resulting in clicks,
+            # for that we smoothen out transition:
+            # if a change in gain is detected (new desired gain != current gain)
+            # it will be reached during this buffer by applying a slowly changing array
+            gain_operation = self._get_smoothener_array(self.gain, new_desired_gain, stream_outputs[0].shape[0])
+        else:
+            gain_operation = self.gain
+
+        self.gain = new_desired_gain
 
         for i in range(0, len(stream_inputs)):
             np.copyto(stream_outputs[i], stream_inputs[i])
-            stream_outputs[i] *= self.gain
+            stream_outputs[i] *= gain_operation
 
     def __init__(self, gain: np.float32):
         super().__init__()
         self.gain: np.float32 = gain
+
+    def _get_smoothener_array(self, initial_gain: float, new_desired_gain: float, num_samples: int) -> np.ndarray:
+        return np.linspace(start=initial_gain, stop=new_desired_gain, num=num_samples, dtype=np.float32)
