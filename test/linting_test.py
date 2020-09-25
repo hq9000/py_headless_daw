add38d321d142804e309909b3675def69aeaeafb
Sergey Grechin <grechin.sergey@gmail.com>
2020-09-25 09:54:08 +0300

    [py_headless_daw-15-mypy #15] mypying tests as well

--- test/linting_test.py
+++ test/linting_test.py
@@ -4,7 +4,7 @@ import os
 
 
 class LintingTest(unittest.TestCase):
-    def test_flake8_main_code(self):
+    def test_flake8_main_code_and_tests(self):
         this_dir = os.path.dirname(os.path.realpath(__file__))
 
         checks_to_ignore = [
@@ -25,7 +25,20 @@ class LintingTest(unittest.TestCase):
         result = subprocess.run(cmd_line_parts)
         self.assertEqual(0, result.returncode, str(result.stdout) + str(result.stderr))
 
-    def test_mypy(self):
+    def test_mypy_main_code_and_tests(self):
+        this_dir = os.path.dirname(os.path.realpath(__file__))
+
+        for source_relative_dir in ["/../py_headless_daw/", ""]:
+            cmd_line_parts = [
+                "mypy",
+                "--ignore-missing-imports",
+                this_dir + source_relative_dir,
+            ]
+
+            result = subprocess.run(cmd_line_parts)
+            self.assertEqual(0, result.returncode, str(result.stdout) + str(result.stderr))
+
+    def test_mypy_test_code(self):
         this_dir = os.path.dirname(os.path.realpath(__file__))
 
         cmd_line_parts = [
