7da100d6aac45008a4d9557f88a043ece724f773
Sergey Grechin <grechin.sergey@gmail.com>
2020-10-30 09:43:26 +0300

    [py_headless_daw-21-create-drum-synthesizer #21] fixed some edge cases in ADSR envelope class

--- py_headless_daw/dsp_utils/adsr_envelope.py
+++ py_headless_daw/dsp_utils/adsr_envelope.py
@@ -7,7 +7,8 @@ from py_headless_daw.dsp_utils.wave_producer_interface import WaveProducerInterf
 # https://en.wikipedia.org/wiki/Envelope_(music)#ADSR
 class ADSREnvelope(WaveProducerInterface):
 
-    def __init__(self, attack_time: float, decay_time: float, sustain_level: float, sustain_time: float, release_time: float):
+    def __init__(self, attack_time: float, decay_time: float, sustain_level: float, sustain_time: float,
+                 release_time: float):
         self.attack_time: float = attack_time
         self.decay_time: float = decay_time
         self.sustain_level: float = sustain_level
@@ -18,25 +19,35 @@ class ADSREnvelope(WaveProducerInterface):
         self.decay_curve: float = 0
         self.release_curve: float = 0
 
-    def produce(self, output_buffer: np.ndarray, sample_rate: int, start_sample: int):
+    def render_to_buffer(self, output_buffer: np.ndarray, sample_rate: int, start_sample: int):
         for i in range(output_buffer.shape[0]):
-            output_buffer[i] = self.get_value(start_sample + i, sample_rate)
+            output_buffer[i] = self.get_one_value(start_sample + i, sample_rate)
 
-    def get_value(self, sample: int, sample_rate: int) -> float:
+    def get_one_value(self, sample: int, sample_rate: int) -> float:
         end_of_attack_sample: int = round(self.attack_time * sample_rate)
         end_of_decay_sample: int = end_of_attack_sample + round(self.decay_time * sample_rate)
         end_of_sustain_sample: int = end_of_decay_sample + round(self.sustain_time * sample_rate)
         end_of_release_sample: int = end_of_sustain_sample + round(self.release_time * sample_rate)
 
         if 0 <= sample <= end_of_attack_sample:
+            if end_of_attack_sample == 0:
+                return 1.0
+
             phase = sample / end_of_attack_sample
             return self._get_curve_value(0, 1.0, self.attack_curve, phase)
         elif end_of_attack_sample < sample <= end_of_decay_sample:
-            phase = (sample - end_of_attack_sample) / (end_of_decay_sample-end_of_attack_sample)
+            if sample == end_of_decay_sample:
+                return self.sustain_level
+            phase = (sample - end_of_attack_sample) / (end_of_decay_sample - end_of_attack_sample)
             return self._get_curve_value(1.0, self.sustain_level, self.decay_curve, phase)
         elif end_of_decay_sample < sample <= end_of_sustain_sample:
+            if sample == end_of_sustain_sample:
+                return self.sustain_level
             return self.sustain_level
         elif end_of_sustain_sample < sample <= end_of_release_sample:
+            if sample == end_of_release_sample:
+                return 0.0
+
             phase = (sample - end_of_sustain_sample) / (end_of_release_sample - end_of_sustain_sample)
             return self._get_curve_value(self.sustain_level, 0, self.release_curve, phase)
         else:
