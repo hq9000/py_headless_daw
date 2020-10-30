49877fad33f0f26079cd9433a7a54e8117810965
Sergey Grechin <grechin.sergey@gmail.com>
2020-10-30 09:17:56 +0300

    [py_headless_daw-21-create-drum-synthesizer #21] testing of putting calculated envelope values into an output buffer

--- test/dsp_utils/adsr_envelope_test.py
+++ test/dsp_utils/adsr_envelope_test.py
@@ -1,5 +1,7 @@
 import unittest
 
+import numpy as np
+
 from py_headless_daw.dsp_utils.adsr_envelope import ADSREnvelope
 
 
@@ -24,6 +26,19 @@ class ADSREnvelopeTestCase(unittest.TestCase):
         self.assertEqual(0.25, envelope.get_value(3500, 1000))
         self.assertEqual(0.0, envelope.get_value(4000, 1000))
         self.assertEqual(0.0, envelope.get_value(4001, 1000))
+
+    def test_generate_wave(self):
+        envelope = ADSREnvelope(
+            attack_time=1,
+            decay_time=1,
+            sustain_level=0.5,
+            sustain_time=1,
+            release_time=1
+        )
+
+        buffer = np.ndarray(shape=(400,), dtype=np.float32)
+        envelope.produce(buffer, 100, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
