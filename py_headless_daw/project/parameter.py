5d104b991069f4466ed962e833e658f1356fb25c
Sergey Grechin <grechin.sergey@gmail.com>
2020-11-13 10:44:59 +0300

    [py_headless_daw-21-create-drum-synthesizer #21] better typing in parameter

--- py_headless_daw/project/parameter.py
+++ py_headless_daw/project/parameter.py
@@ -2,25 +2,36 @@ from typing import List, Tuple, Union, cast
 
 from py_headless_daw.project.value_provider_consumer import ValueConsumer
 
+ParameterRangeType = Union[Tuple[float, float], List[str]]
+ParameterValueType = Union[float, str]
+
 
 class Parameter(ValueConsumer):
     TYPE_FLOAT = 'float'
     TYPE_ENUM = 'enum'
 
-    def __init__(self, name: str, value: Union[float, str], parameter_type: str,
-                 parameter_range: Union[Tuple[float, float], List[str]]):
+    def __init__(self, name: str, value: ParameterValueType, parameter_type: str,
+                 parameter_range: ParameterRangeType):
         super().__init__()
         self.name: str = name
-        self.value: Union[float, str] = value
+        if parameter_type not in self.get_available_types():
+            raise ValueError(
+                f"unsupported parameter type {parameter_type}, supported: {', '.join(self.get_available_types())}")
+
         self.type: str = parameter_type
         self.range = parameter_range
+        self.value: ParameterValueType = value
 
     @property
     def value(self):
         return self.value
 
+    @classmethod
+    def get_available_types(cls) -> List[str]:
+        return [cls.TYPE_FLOAT, cls.TYPE_ENUM]
+
     @value.setter
-    def value(self, new_value: Union[float, str]):
+    def value(self, new_value: ParameterValueType):
         if self.type == self.TYPE_FLOAT:
             value_range = cast(self.range, Tuple[float, float])
             value = cast(new_value, float)
@@ -48,3 +59,30 @@ class Parameter(ValueConsumer):
                     f"{new_value}, a value for parameter {self.name} should have been one of {', '.join(value_range)}")
 
             self.value = new_value
+
+    @property
+    def range(self) -> ParameterRangeType:
+        return self.range
+
+    @range.setter
+    def range(self, value: ParameterValueType):
+        if self.type == self.TYPE_FLOAT:
+            if isinstance(value, tuple):
+                if len(value) != 2:
+                    raise ValueError('ranges for float params must be tuples of length 2 (error: a27c90e2)')
+                for i in range(2):
+                    if not isinstance(value[i], float):
+                        raise ValueError('ranges for float params must be tuples of floats (error: 80d5db89)')
+            else:
+                raise ValueError('for float params, range must be a tuple of two floats (error: e37e6f14)')
+        elif self.type == self.TYPE_ENUM:
+            if isinstance(value, list):
+                for e in value:
+                    if not isinstance(e, str):
+                        raise ValueError('for enum params, each element range must be a string (error: f0dc465c)')
+            else:
+                raise ValueError('for enum params, range must be a list of strings (error: a9c603f3)')
+        else:
+            raise ValueError('unsupported type (error: 2878e15c)')
+
+        self.range = value
