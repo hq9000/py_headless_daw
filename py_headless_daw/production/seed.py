a877f41ef0ed6736e610898ba0a4bd0cbeb98b4b
Sergey Grechin <grechin.sergey@gmail.com>
2020-11-23 09:59:06 +0300

    [py_headless_daw-23-simple-edm-v0 #23] implementing choose_one

--- py_headless_daw/production/seed.py
+++ py_headless_daw/production/seed.py
@@ -2,7 +2,7 @@ from random import randint, Random
 from typing import Any, Dict, TypeVar
 
 T = TypeVar('T')
-RandomChoiceOptions = Dict[T, float]
+RandomChoiceOptions = Dict[T, int]
 
 
 class Seed:
@@ -22,5 +22,22 @@ class Seed:
         local_generator = Random(self._seed + sub_seed)
         return local_generator.randint(min_val, max_val)
 
-    def choose_one(self, sub_seed: str, probabilities: RandomChoiceOptions) -> T:
+    def choose_one(self, probabilities: RandomChoiceOptions, sub_seed: str) -> T:
+        sum_of_probabilities: int = 0
+
+        # probabilities do not have to sum up to 100 or anything,
+        # it's enough if they are positive integers
+        for option, add_probability in probabilities.items():
+            sum_of_probabilities += add_probability
+
+        dice_value = self.randint(0, sum_of_probabilities, sub_seed)
+
+        sum_of_probabilities = 0
+        for option, add_probability in probabilities.items():
+            if sum_of_probabilities <= dice_value < sum_of_probabilities + add_probability:
+                return option
+
+        raise ValueError('unable to generate a random choice (error: 8256cd68)')
+
+
         return list(probabilities.keys())[0]  # dummy impl
--- py_headless_daw/production/simple_edm/simple_edm_producer.py
+++ py_headless_daw/production/simple_edm/simple_edm_producer.py
@@ -1,18 +1,61 @@
+from typing import List
+
 from py_headless_daw.production.producer_interface import ProducerInterface
 from py_headless_daw.production.seed import Seed
+from py_headless_daw.project.audio_track import AudioTrack
+from py_headless_daw.project.midi_track import MidiTrack
+from py_headless_daw.project.plugins.drum_synth_plugin import DrumSynthPlugin
 from py_headless_daw.project.project import Project
 
 
 class SimpleEdmProducer(ProducerInterface):
-    def generate_project(self, seed: Seed) -> Project:
-        number_of_synth_tracks = self._think_of_number_of_synth_track(seed)
 
-    def _think_of_number_of_synth_track(self, seed: Seed) -> int:
-        return seed.choose_one(
-            "num synth tracks",
+    def __init__(self, seed: Seed):
+        self._seed: Seed = seed
+
+    def generate_project(self) -> Project:
+        number_of_synth_tracks = self._invent_number_of_synth_tracks()
+
+        master_track = AudioTrack()
+
+        bd_track = self.generate_bd_track()
+        master_track.add_input(bd_track)
+
+        for i in range(0, number_of_synth_tracks):
+            master_track.add_input(self._generate_synth_track(i))
+
+        return Project(master_track)
+
+    def _invent_number_of_synth_tracks(self) -> int:
+        return self._seed.choose_one(
             {
-                2: 20.0,
-                3: 20.0,
-                4: 20.0
-            }
+                2: 20,
+                3: 20,
+                4: 20
+            },
+            "num synth tracks"
         )
+
+    def generate_bd_track(self) -> AudioTrack:
+        """
+        bd track is a midi track feeding itself to drum synth track
+        """
+
+        drum_synth_audio_track = self._generate_drum_synth_audio_track_itself()
+        drum_synth_midi_track = self._generate_drum_synth_midi_track()
+
+        drum_synth_audio_track.add_input(drum_synth_midi_track)
+        return drum_synth_audio_track
+
+    def _generate_synth_track(self, i: int) -> AudioTrack:
+        pass
+
+    def _generate_drum_synth_audio_track_itself(self) -> AudioTrack:
+        res = AudioTrack
+        plugin = DrumSynthPlugin()
+        res.plugins = [plugin]
+
+        pass
+
+    def _generate_drum_synth_midi_track(self) -> MidiTrack:
+        pass
