2e8f7717170f59f8c665a63c48e499c705951671
Sergey Grechin <grechin.sergey@gmail.com>
2020-11-24 06:21:06 +0300

    [py_headless_daw-23-simple-edm-v0 #23] blind rendering of the project in tests

--- test/production/simple_edm/simple_edm_producer_test.py
+++ test/production/simple_edm/simple_edm_producer_test.py
@@ -1,14 +1,23 @@
 import unittest
 
+from py_headless_daw.application_container import ApplicationContainer
 from py_headless_daw.production.seed import Seed
 from py_headless_daw.production.simple_edm.simple_edm_producer import SimpleEdmProducer
+from py_headless_daw.project.project_renderer import ProjectRenderer
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         seed = Seed('hello')
         producer = SimpleEdmProducer(seed)
-        res = producer.generate_project()
+        project = producer.generate_project()
+
+        container = ApplicationContainer()
+
+        compiler = container.project_compiler()
+        renderer = ProjectRenderer(compiler)
+
+        renderer.render_to_array(project, project.start_time_seconds, project.end_time_seconds)
 
 
 if __name__ == '__main__':
