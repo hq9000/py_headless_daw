024dada4122120d0e62752c4ae7c710780c27af9
Sergey Grechin <grechin.sergey@gmail.com>
2020-12-10 05:34:19 +0300

    [py_headless_daw-23-simple-edm-v0 #23] made SEDM test parametrized

--- test/production/simple_edm/simple_edm_producer_test.py
+++ test/production/simple_edm/simple_edm_producer_test.py
@@ -1,16 +1,22 @@
 import unittest
 from pathlib import Path
+
+import parametrized
+
 from py_headless_daw.application_container import ApplicationContainer
 from py_headless_daw.production.seed import Seed
 from py_headless_daw.production.simple_edm.simple_edm_producer import SimpleEdmProducer
 from py_headless_daw.project.project_renderer import ProjectRenderer
 
-# import matplotlib.pyplot as plt
 
-
+@parametrized.parametrized_test_case
 class SimpleEdmProducerTest(unittest.TestCase):
-    def test_something(self):
-        seed = Seed('hello')
+
+    @parametrized.parametrized([
+        ['hello'], ['hello1'], ['hello2']
+    ])
+    def generate_audio(self, seed_string: str):
+        seed = Seed(seed_string)
         producer = SimpleEdmProducer(seed)
         project = producer.generate_project()
 
@@ -19,11 +25,8 @@ class SimpleEdmProducerTest(unittest.TestCase):
         compiler = container.project_compiler()
         renderer = ProjectRenderer(compiler)
 
-        out_file_path = str(Path(__file__).parent.absolute()) + "/output/out.wav"
+        out_file_path = str(Path(__file__).parent.absolute()) + f"/output/out_{seed_string}.wav"
         renderer.render_to_file(project, project.start_time_seconds, project.end_time_seconds, out_file_path)
-
-        #plt.plot(output[1][0:400000])
-        #plt.show()
 
         pass
 
